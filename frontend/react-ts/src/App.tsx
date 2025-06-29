264031745017d097e5aee67c7122261721a94c81
RMalinowski <rafal.m3211@gmail.com>
2025-06-29 21:51:29 +0200

    feature: make background

--- frontend/react-ts/src/App.tsx
+++ frontend/react-ts/src/App.tsx
@@ -1,4 +1,5 @@
 import { Container, Box, Typography } from "@mui/material";
+import nubisoftBgLogo from "./assets/nubisoftTransparent.svg";
 import nubisoftLogo from "./assets/nubisoft.svg";
 import AppThemeProvider from "./context/theme/theme";
 import WeatherCard from "./components/WeatherCard/WeatherCard";
@@ -6,27 +7,41 @@ import WeatherCard from "./components/WeatherCard/WeatherCard";
 function App() {
   return (
     <AppThemeProvider>
-      <Container maxWidth="lg">
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "2em",
-          }}
-        >
-          <a href="https://nubisoft.io/" target="_blank">
-            <img src={nubisoftLogo} className="" alt="Nubisoft logo" />
-          </a>
-          <Typography variant="h2" component="h1">
-            NubiWeather
-          </Typography>
-        </Box>
-        <Box sx={{ mt: "3em", display: "flex", gap: "3em" }}>
-          <WeatherCard />
-          <WeatherCard />
-        </Box>
-      </Container>
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+
+          background: `url(${nubisoftBgLogo}) no-repeat  -10vw 15vh`,
+          backgroundSize: "contain",
+        }}
+      >
+        <Container maxWidth="lg">
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "2em",
+            }}
+          >
+            <a href="https://nubisoft.io/" target="_blank">
+              <img src={nubisoftLogo} className="" alt="Nubisoft logo" />
+            </a>
+            <Typography variant="h2" component="h1">
+              NubiWeather
+            </Typography>
+          </Box>
+          <Box sx={{ mt: "3em", display: "flex", gap: "3em" }}>
+            <WeatherCard />
+            <WeatherCard />
+          </Box>
+        </Container>
+      </Box>
     </AppThemeProvider>
   );
 }
