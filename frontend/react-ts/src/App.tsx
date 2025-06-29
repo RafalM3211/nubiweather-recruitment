d501a8d0d88dd4dff82e88b9611cb88d11ab3324
RMalinowski <rafal.m3211@gmail.com>
2025-06-29 20:13:15 +0200

    chore: configure material UI

--- frontend/react-ts/src/App.tsx
+++ frontend/react-ts/src/App.tsx
@@ -1,19 +1,24 @@
+import { Button } from "@mui/material";
 import nubisoftLogo from "./assets/nubisoft.svg";
+import AppThemeProvider from "./context/theme/theme";
 
 function App() {
   return (
-    <div className="flex justify-center flex-col gap-4 items-center">
-      <div>
-        <a href="https://nubisoft.io/" target="_blank">
-          <img src={nubisoftLogo} className="" alt="Nubisoft logo" />
-        </a>
+    <AppThemeProvider>
+      <div className="flex justify-center flex-col gap-4 items-center">
+        <div>
+          <a href="https://nubisoft.io/" target="_blank">
+            <img src={nubisoftLogo} className="" alt="Nubisoft logo" />
+          </a>
+        </div>
+        <h1>NubiWeather</h1>
+        <section>
+          <h2>Here's your starting point.</h2>
+          <h3>Good luck! :)</h3>
+          <Button>bytton</Button>
+        </section>
       </div>
-      <h1>NubiWeather</h1>
-      <section>
-        <h2>Here's your starting point.</h2>
-        <h3>Good luck! :)</h3>
-      </section>
-    </div>
+    </AppThemeProvider>
   );
 }
 
